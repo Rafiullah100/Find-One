53f30ca7a1cc092621a7e464a184f15c4cec7c0e
Rafi ullah <rafiullah@codeforpakistan.org>
2024-03-19 15:17:11 +0500

    changes

---
+++ Find One/Module/SideMenu/FilterModule/Cell/FilterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCollectionViewCell.swift
+//  Find One
+//
+//  Created by MacBook Pro on 3/19/24.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Find One/Module/SideMenu/FilterModule/ViewModel/FilterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FilterViewModel.swift
+//  Find One
+//
+//  Created by MacBook Pro on 3/19/24.
+//
+
+import Foundation
